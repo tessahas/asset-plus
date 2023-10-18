c228822017f4f3702d8f156f49c7f0faf76324f1
LuisAtMcGill <116434210+LuisAtMcGill@users.noreply.github.com>
2023-10-18 17:56:12 -0400

    Update DeleteEmployeeStepDefinitions.java
    Definitions written by Kevin and Yuri, & cie.

--- src/test/java/ca/mcgill/ecse/assetplus/features/DeleteEmployeeStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/assetplus/features/DeleteEmployeeStepDefinitions.java
@@ -1,22 +1,28 @@
 package ca.mcgill.ecse.assetplus.features;
-
+import ca.mcgill.ecse.assetplus.model.*;
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
 
 public class DeleteEmployeeStepDefinitions {
+
+  private AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
 
   @Given("the following employees exist in the system \\(p1)")
   public void the_following_employees_exist_in_the_system_p1(
       io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+    List<Map<String, String>> rows = dataTable.asMaps();
+    for (var row: rows) {
+      String email = row.get("email");
+      String name = row.get("name");
+      String password = row.get("password");
+      String phoneNumber = row.get("phoneNumber");
+      new Employee(email, name, password, phoneNumber, assetPlus);
+    }
   }
 
   @Given("the following manager exists in the system \\(p1)")
@@ -37,22 +43,32 @@ public class DeleteEmployeeStepDefinitions {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
-
+  
+  /**
+   * @author Jerome Desrosiers
+   */
   @Then("the employee account linked to {string} shall not exist in the system \\(p1)")
   public void the_employee_account_linked_to_shall_not_exist_in_the_system_p1(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    
+    User actualEmployee = Employee.getWithEmail(string);
+    Assertions.assertEquals(null, actualEmployee);
   }
-
+  /**
+   * @author Yuri Sorice
+   */
   @Then("the manager account linked to {string} shall exist in the system \\(p1)")
   public void the_manager_account_linked_to_shall_exist_in_the_system_p1(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    User expectedManager = Manager.getWithEmail(string);
+    User actualManager = assetPlus.getManager();
+    Assertions.assertEquals(expectedManager, actualManager);
   }
-
+  /**
+   * @author Tessa Hason
+   */
   @Then("the number of employees in the system shall be {string} \\(p1)")
   public void the_number_of_employees_in_the_system_shall_be_p1(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    int expectedNumOfEmployees = Integer.parseInt(string);
+    int actualNumOfEmployees = assetPlus.numberOfGuests();
+    Assertions.assertEquals(expectedNumOfEmployees,actualNumOfEmployees);
   }
 }
