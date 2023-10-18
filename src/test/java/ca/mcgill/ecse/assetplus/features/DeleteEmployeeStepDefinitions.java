ca2a013e1a24dbdeeedd5876dd247f3b503a4f1f
LuisAtMcGill <116434210+LuisAtMcGill@users.noreply.github.com>
2023-10-18 18:03:19 -0400

    Update DeleteEmployeeStepDefinitions.java
    the Yuri backup from Oct 18th 5:33PM

--- src/test/java/ca/mcgill/ecse/assetplus/features/DeleteEmployeeStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/assetplus/features/DeleteEmployeeStepDefinitions.java
@@ -1,16 +1,16 @@
 package ca.mcgill.ecse.assetplus.features;
-
+import ca.mcgill.ecse.assetplus.model.*;
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.util.List;
 import java.util.Map;
-import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
-import ca.mcgill.ecse.assetplus.model.*;
+import org.junit.jupiter.api.Assertions;
 
 public class DeleteEmployeeStepDefinitions {
 
-  private AssetPlus assetplus = AssetPlusApplication.getAssetPlus();
+  private AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
 
   @Given("the following employees exist in the system \\(p1)")
   public void the_following_employees_exist_in_the_system_p1(
@@ -21,7 +21,7 @@ public class DeleteEmployeeStepDefinitions {
       String name = row.get("name");
       String password = row.get("password");
       String phoneNumber = row.get("phoneNumber");
-      new Employee(email, name, password, phoneNumber, assetplus);
+      new Employee(email, name, password, phoneNumber, assetPlus);
     }
   }
 
@@ -41,24 +41,34 @@ public class DeleteEmployeeStepDefinitions {
   @When("the employee attempts to delete their own account linked to the {string} \\(p1)")
   public void the_employee_attempts_to_delete_their_own_account_linked_to_the_p1(String string) {
     // Write code here that turns the phrase above into concrete actions
-    ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet6Controller.deleteEmployeeOrGuest(string);
+    throw new io.cucumber.java.PendingException();
   }
-
+  
+  /**
+   * @author Jerome Desrosiers
+   */
   @Then("the employee account linked to {string} shall not exist in the system \\(p1)")
   public void the_employee_account_linked_to_shall_not_exist_in_the_system_p1(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    
+    User actualEmployee = Employee.getWithEmail(string);
+    Assertions.assertEquals(null, actualEmployee);
   }
-
+  /**
+   * @author Yuri Sorice
+   */
   @Then("the manager account linked to {string} shall exist in the system \\(p1)")
   public void the_manager_account_linked_to_shall_exist_in_the_system_p1(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    User expectedManager = Manager.getWithEmail(string);
+    User actualManager = assetPlus.getManager();
+    Assertions.assertEquals(expectedManager, actualManager);
   }
-
+  /**
+   * @author Tessa Hason
+   */
   @Then("the number of employees in the system shall be {string} \\(p1)")
   public void the_number_of_employees_in_the_system_shall_be_p1(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    int expectedNumOfEmployees = Integer.parseInt(string);
+    int actualNumOfEmployees = assetPlus.numberOfGuests();
+    Assertions.assertEquals(expectedNumOfEmployees,actualNumOfEmployees);
   }
 }
