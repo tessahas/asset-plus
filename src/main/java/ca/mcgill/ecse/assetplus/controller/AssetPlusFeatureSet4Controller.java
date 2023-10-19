d03a77e3f295b0426246bb4817f9a58f2b4bc4bb
YuriSorice <143831174+YuriSorice@users.noreply.github.com>
2023-10-19 09:27:43 -0400

    Update AssetPlusFeatureSet4Controller.java

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet4Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet4Controller.java
@@ -4,9 +4,24 @@ import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 
 import java.sql.Date;
 
-
+/** <h1>AssetPlusFeature4Controller</h1>
+ * @version 1.0
+ * @author Yuri Sorice
+ * @since 2023-10-18
+ */
 public class AssetPlusFeatureSet4Controller {
-  // assetNumber -1 means that no asset is specified
+  /**
+   * 
+   * <h2>addMaintenanceTicket</h2>
+   * This method is used to add a maintenance ticket to assetPlus.
+   * @param id The id number of the maintenance ticket
+   * @param raisedOnDate The date the ticket was made
+   * @param description A description of what is in need of maintenance
+   * @param email The ticker raiser's email
+   * @param assetNumber The number of the asset associated with the ticket.
+   * @return Returns a string that is empty if addMaintenanceTicket was successful and
+   * no errors were raised. Returns a string with an error message otherwise.
+   */
   public static String addMaintenanceTicket(int id, Date raisedOnDate, String description,
       String email, int assetNumber) {
         String errorMessage = "";
@@ -45,7 +60,18 @@ public class AssetPlusFeatureSet4Controller {
       return (errorMessage);
   }
 
-  // newAssetNumber -1 means that no asset is specified
+  /**
+   * <h3>updateMaintenanceTicket</h3>
+   * This method updates an existing maintenance ticket.
+   * @param id The ticket id of the existing ticket
+   * @param newRaisedOnDate The date of the update made to the ticket
+   * @param newDescription The new description of what kind of maintenance is required
+   * @param newEmail The email of the ticket updater
+   * @param newAssetNumber The new number of the object (if the number is -1 the ticket stays assigned
+   * to the same asset)
+   * @return Similar to addMaintenanceTicket, it returns an empty string if the method is successful,
+   * otherwise it returns the error message in the form of a string.
+   */
   public static String updateMaintenanceTicket(int id, Date newRaisedOnDate, String newDescription,
       String newEmail, int newAssetNumber) {
         String errorMessage = "";
@@ -58,6 +84,7 @@ public class AssetPlusFeatureSet4Controller {
         if (!SpecificAsset.hasWithAssetNumber(newAssetNumber)) {
           errorMessage += "The asset does not exist.";
         }
+        if (!errorMessage.isEmpty()) return errorMessage;
         MaintenanceTicket ticketToEdit = MaintenanceTicket.getWithId(id);
         ticketToEdit.setRaisedOnDate(newRaisedOnDate);
         ticketToEdit.setDescription(newDescription);
@@ -70,7 +97,12 @@ public class AssetPlusFeatureSet4Controller {
         return errorMessage;
     }
         
-
+  /**
+   * <h4>deleteMaintenanceTicket</h4>
+   * This method deletes a maintenance ticket with the provided ticket id.
+   * @param id The number of the ticket that is to be deleted
+   * If the id given does not have a corresponding ticket, then nothing happens.
+   */
   public static void deleteMaintenanceTicket(int id) {
 
     if (MaintenanceTicket.hasWithId(id)) {
