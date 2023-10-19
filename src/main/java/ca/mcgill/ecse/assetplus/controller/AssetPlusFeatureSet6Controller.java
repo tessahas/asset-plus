d0bccc8426179b471a09f400d8ca5ce2d18be62b
Tessa Hason <tessa.hason@mail.mcgill.ca>
2023-10-19 01:51:44 -0400

    Finished feature set 6 controller

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
@@ -1,11 +1,24 @@
 package ca.mcgill.ecse.assetplus.controller;
 import java.util.List;
+import java.util.ArrayList;
+import java.sql.Date;
 import ca.mcgill.ecse.assetplus.model.*;
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
-import java.util.Map;
+
+/**
+ * @author Tessa Hason
+ * @version 1.0
+ * @since 2023-10-18
+ */
 
 
 public class AssetPlusFeatureSet6Controller {
+
+  /**
+   * @author Tessa Hason
+   * This method is used to delete an Employee or a Guest within AssetPlus.
+   * @param email The employee or guest with the given email must be deleted.
+   */
 
   public static void deleteEmployeeOrGuest(String email) {
 
@@ -27,10 +40,66 @@ public class AssetPlusFeatureSet6Controller {
     }
   }
 
-  // returns all tickets
+  /**
+   * @author Tessa Hason
+   * This method is used to view the status of Maintenance Tickets for an employee/manager.
+   * @return Returns a list of TOMaintenanceTicket objects representing all MaintenanceTickets
+   * in the AssetPlus System.
+   */
+
   public static List<TOMaintenanceTicket> getTickets() {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
+
+    if (assetPlus.hasMaintenanceTickets()){
+      List<MaintenanceTicket> maintenanceTickets = assetPlus.getMaintenanceTickets();
+      List<TOMaintenanceTicket> TOMaintenanceTickets = new ArrayList<TOMaintenanceTicket>();
+
+      for (MaintenanceTicket ticket : maintenanceTickets) {
+        int id = ticket.getId();
+        Date date = ticket.getRaisedOnDate();
+        String description = ticket.getDescription();
+        String ticketRaiserEmail = ticket.getTicketRaiser().getEmail();
+        String assetName = ticket.getAsset().getAssetType().getName();
+        int lifespan = ticket.getAsset().getAssetType().getExpectedLifeSpan();
+        Date purchaseDate = ticket.getAsset().getPurchaseDate();
+        int floorNumber = ticket.getAsset().getFloorNumber();
+        int roomNumber = ticket.getAsset().getRoomNumber();
+        List<String> urls = null;
+        List<TOMaintenanceNote> notes = null;
+
+
+        if (ticket.hasTicketImages()){
+          urls = new ArrayList<String>();
+          for (int i=0; i<ticket.numberOfTicketImages(); i++){
+            String currUrl = ticket.getTicketImage(i).getImageURL();
+            urls.add(currUrl);
+          }
+        }
+
+        if (ticket.hasTicketNotes()) {
+          List<MaintenanceNote> maintenanceNotes = ticket.getTicketNotes();
+          for (MaintenanceNote note : maintenanceNotes) {
+            Date noteDate = note.getDate();
+            String noteDescription = note.getDescription();
+            String noteTakerEmail = note.getNoteTaker().getEmail();
+
+            // Create a TOMaintenanceNote
+            TOMaintenanceNote TOMaintenanceNote = new TOMaintenanceNote(noteDate, noteDescription, noteTakerEmail);
+            notes.add(TOMaintenanceNote);
+          }
+        }
+
+
+        TOMaintenanceTicket TOticket = new TOMaintenanceTicket(id, date, description, ticketRaiserEmail, assetName, lifespan, purchaseDate, floorNumber, roomNumber, urls, notes.toArray(new TOMaintenanceNote[0]));
+        TOMaintenanceTickets.add(TOticket);
+
+      }
+
+      return TOMaintenanceTickets;
+
+    }
+    return null;
+    
   }
 
 }
