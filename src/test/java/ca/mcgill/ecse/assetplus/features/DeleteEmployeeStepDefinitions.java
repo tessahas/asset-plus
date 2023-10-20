20b3915e7b5df2261abce76184fa2ff9cbb73f73
YuriSorice <143831174+YuriSorice@users.noreply.github.com>
2023-10-19 20:42:28 -0400

    Update DeleteEmployeeStepDefinitions.java

--- src/test/java/ca/mcgill/ecse/assetplus/features/DeleteEmployeeStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/assetplus/features/DeleteEmployeeStepDefinitions.java
@@ -15,11 +15,11 @@ public class DeleteEmployeeStepDefinitions {
   /**
    * This step checks if the following employees exist in the system.
    * @author Kevin Li
-   * @param dataTable Table containing the information on the employees we want to exist in the system.
+   * @param dataTable This is a data table containing all the employees that exists in the system.
    */
   @Given("the following employees exist in the system \\(p1)")
   public void the_following_employees_exist_in_the_system_p1(
-      io.cucumber.datatable.DataTable dataTable) {
+          io.cucumber.datatable.DataTable dataTable) {
     List<Map<String, String>> rows = dataTable.asMaps();
     for (var row: rows) {
       String email = row.get("email");
@@ -37,7 +37,7 @@ public class DeleteEmployeeStepDefinitions {
    */
   @Given("the following manager exists in the system \\(p1)")
   public void the_following_manager_exists_in_the_system_p1(
-      io.cucumber.datatable.DataTable dataTable) {
+          io.cucumber.datatable.DataTable dataTable) {
     List<Map<String, String>> rows = dataTable.asMaps();
     for (var row: rows) {
       String email = row.get("email");
@@ -49,45 +49,44 @@ public class DeleteEmployeeStepDefinitions {
   /**
    * This step deletes an employee account linked to a particular email string.
    * @author Luis Jarquin
-   * @param string This is a string containing the email address linked ot an employee account.
+   * @param employeeEmail This is a string containing the email address linked ot an employee account.
    */
   @When("the employee attempts to delete their own account linked to the {string} \\(p1)")
-  public void the_employee_attempts_to_delete_their_own_account_linked_to_the_p1(String string) {
-    AssetPlusFeatureSet6Controller.deleteEmployeeOrGuest(string);
+  public void the_employee_attempts_to_delete_their_own_account_linked_to_the_p1(String employeeEmail) {
+    AssetPlusFeatureSet6Controller.deleteEmployeeOrGuest(employeeEmail);
   }
-  
+
   /**
    * This step checks that once the employee with email string attempts to delete their own account, their account does not exist in the system.
    * @author Jerome Desrosiers
-   * @param string This is a string containing the email address that was linked to the now deleted employee account.
+   * @param employeeEmail This is a string containing the email address that was linked to the now deleted employee account.
    */
   @Then("the employee account linked to {string} shall not exist in the system \\(p1)")
-  public void the_employee_account_linked_to_shall_not_exist_in_the_system_p1(String string) {
-    User actualEmployee = Employee.getWithEmail(string);
-    if(actualEmployee == null){
-    }else{
+  public void the_employee_account_linked_to_shall_not_exist_in_the_system_p1(String employeeEmail) {
+    User actualEmployee = Employee.getWithEmail(employeeEmail);
+    if (actualEmployee != null) {
       Assertions.assertFalse(actualEmployee instanceof Employee);
     }
   }
   /**
    * This step checks that after the employee with email string attempts to delete their own account, the manager still exists in the system.
    * @author Yuri Sorice
-   * @param string This is a string containing the email address of the manager.
+   * @param managerEmail This is a string containing the email address of the manager.
    */
   @Then("the manager account linked to {string} shall exist in the system \\(p1)")
-  public void the_manager_account_linked_to_shall_exist_in_the_system_p1(String string) {
-    User expectedManager = Manager.getWithEmail(string);
+  public void the_manager_account_linked_to_shall_exist_in_the_system_p1(String managerEmail) {
+    User expectedManager = Manager.getWithEmail(managerEmail);
     User actualManager = assetPlus.getManager();
     Assertions.assertEquals(expectedManager, actualManager);
   }
   /**
    * This step checks that the number of employees in the system is correct after deletion of an employee with email string.
    * @author Tessa Hason
-   * @param string This is a string containing the expected number of employees.
+   * @param numOfEmployees This is a string containing the expected number of employees.
    */
   @Then("the number of employees in the system shall be {string} \\(p1)")
-  public void the_number_of_employees_in_the_system_shall_be_p1(String string) {
-    int expectedNumOfEmployees = Integer.parseInt(string);
+  public void the_number_of_employees_in_the_system_shall_be_p1(String numOfEmployees) {
+    int expectedNumOfEmployees = Integer.parseInt(numOfEmployees);
     int actualNumOfEmployees = assetPlus.numberOfEmployees();
     Assertions.assertEquals(expectedNumOfEmployees,actualNumOfEmployees);
   }
