21f9f57a93502a91c5922f7d8a5b0704139137cd
tessahas <tessa.hason@mail.mcgill.ca>
2023-11-08 21:17:18 -0500

    Update AssetPlusAPI.java

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusAPI.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusAPI.java
@@ -27,4 +27,30 @@ public class AssetPlusAPI {
     }
     return errorMessage;
   }
+
+  public static String disapproveTicketWork(MaintenanceTicket toDisapprove){
+    String errorMessage = "";
+    if (!MaintenanceTicket.hasWithId(toDisapprove.getId())) {
+      errorMessage += "Maintenance ticket does not exist.";
+    }
+    else if(toDisapprove.getTicketStatusFullName().equalsIgnoreCase("open")) {
+      errorMessage += "Cannot disapprove a maintenance ticket which is open.";
+    }
+    else if(toDisapprove.getTicketStatusFullName().equalsIgnoreCase("Assigned")) {
+      errorMessage += "Cannot disapprove a maintenance ticket which is assigned.";
+    }
+    else if(toDisapprove.getTicketStatusFullName().equalsIgnoreCase("Closed")) {
+      errorMessage += "Cannot disapprove a maintenance ticket which is closed.";
+    }
+    else if(toDisapprove.getTicketStatusFullName().equalsIgnoreCase("InProgress")) {
+      errorMessage += "Cannot disapprove a maintenance ticket which is in progress.";
+    }
+    try {
+      toDisapprove.disapproveWork();
+    }
+    catch(Exception e) {
+      errorMessage += "Some unknown error.";
+    }
+    return errorMessage;
+  }
 }
