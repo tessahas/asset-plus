9c0bee63095e445b9fffbb34ed0046fa86532dff
YuriSorice <yuri.sorice@mail.mcgill.ca>
2023-11-30 11:07:29 -0500

    porgrers

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/MaintenancePageController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/MaintenancePageController.java
@@ -14,11 +14,13 @@ import java.sql.Date;
 // making/updating maintenance tickets do not require any of the choiceboxes, need to remove them from scenebuilder and here
 // send the choiceboxes to assignment page and its controller
 public class MaintenancePageController {
-  @FXML private TextField createTicketIdTextField, createTicketDateTextField, createTicketDescriptionTextField, createTicketAssetNumberTextField,
-        updateTicketIdTextField, updateTicketDateTextField, updateTicketDescriptionTextField,
+  @FXML private TextField createTicketIdTextField, createTicketRaiserEmailTextField, createTicketDateTextField, createTicketDescriptionTextField, createTicketAssetNumberTextField,
+        updateTicketIdTextField, updateTicketRaiserEmailTextField, updateTicketDateTextField, updateTicketDescriptionTextField, updateTicketAssetNumberTextField,
         imageTicketIdTextField, addImageURLTextField, deleteImageURLTextField;
-@FXML private Button createButton, updateButton, deleteButton, addImageButton, deleteImageButton;
-@FXML private ChoiceBox<String> createTicketPriorityChoiceBox, createTicketTimeEstimateChoiceBox, createTicketApprovalChoiceBox,
+
+  @FXML private Button createButton, updateButton, deleteButton, addImageButton, deleteImageButton;
+
+  @FXML private ChoiceBox<String> createTicketPriorityChoiceBox, createTicketTimeEstimateChoiceBox, createTicketApprovalChoiceBox,
         updateTicketPriorityChoiceBox, updateTicketTimeEstimateChoiceBox, updateTicketApprovalChoiceBox;
 
 
@@ -71,13 +73,17 @@ public class MaintenancePageController {
   public void deleteImageClicked(ActionEvent event) {
     String name = deleteImageURLTextField.getText();
     String idString = imageTicketIdTextField.getText();
+
     if (name == null || name.trim().isEmpty()) {
       ViewUtils.showError("Please input a valid image URL");
-    } else if (idString == null || idString.trim().isEmpty()) {
+    } 
+    else if (idString == null || idString.trim().isEmpty()) {
       ViewUtils.showError("Please input a valid ticket ID");
-    } else {
+    } 
+    else {
       // reset the text field if success
       int id = Integer.parseInt(idString);
+
       if (ViewUtils.successful(AssetPlusFeatureSet5Controller.deleteImageFromMaintenanceTicket(name,id))) {
         deleteImageURLTextField.setText("");
       }
@@ -91,6 +97,7 @@ public class MaintenancePageController {
     String dateString = createTicketDateTextField.getText();
     String description = createTicketDescriptionTextField.getText();
     String assetString = createTicketAssetNumberTextField.getText();
+    String emailString = createTicketRaiserEmailTextField.getText();
 
     // Checking for null or empty strings
     if (idString == null || idString.trim().isEmpty()) {
@@ -105,20 +112,80 @@ public class MaintenancePageController {
     else if (assetString == null || assetString.trim().isEmpty()) {
       ViewUtils.showError("Please input a valid asset number");
     }
+    else if (emailString == null || emailString.trim().isEmpty()) {
+      ViewUtils.showError("Please input a valid user email");
+    }
     else {
       int id = Integer.parseInt(idString);
       Date date = Date.valueOf(dateString);
       int assetNumber = Integer.parseInt(assetString);
       // TODO user email may not always be the manager
-      if(ViewUtils.successful(AssetPlusFeatureSet4Controller.addMaintenanceTicket(id, date, description,"manager@ap.com", assetNumber))) {
+      String errorMessage = AssetPlusFeatureSet4Controller.addMaintenanceTicket(id, date, description, emailString, assetNumber);
         // clear textfields
+      if (errorMessage.isEmpty()) {
         createTicketIdTextField.setText(null);
         createTicketDateTextField.setText(null);
         createTicketDescriptionTextField.setText(null);
+        createTicketRaiserEmailTextField.setText(null);
+        createTicketAssetNumberTextField.setText(null);
+
+      } else {
+        ViewUtils.showError(errorMessage);
       }
+
+
     }
+  }
+  // Event Listener on Button[#updateButton].onAction
+  @FXML
+  public void updateTicket(ActionEvent event) {
+    String idString = updateTicketIdTextField.getText();
+    String dateString = updateTicketDateTextField.getText();
+    String description = updateTicketDescriptionTextField.getText();
+    String email = updateTicketRaiserEmailTextField.getText();
+    String assetString = updateTicketAssetNumberTextField.getText();
+
+    // Checking for null or empty strings
+    if (idString == null || idString.trim().isEmpty()) {
+      ViewUtils.showError("Please input a valid ticket id");
+    }
+    else if (dateString == null || dateString.trim().isEmpty()) {
+      ViewUtils.showError("Please input a valid date");
+    }
+    else if (description == null || description.trim().isEmpty()) {
+      ViewUtils.showError("Please input a valid description");
+    }
+    else if (assetString == null || assetString.trim().isEmpty()) {
+      ViewUtils.showError("Please input a valid asset number");
+    }
+    else if (email == null || email.trim().isEmpty()) {
+      ViewUtils.showError("Please input a valid user email");
+    }
+    else {
+      int id = Integer.parseInt(idString);
+      Date date = Date.valueOf(dateString);
+      int assetNumber = Integer.parseInt(assetString);
+      String errorMessage = AssetPlusFeatureSet4Controller.updateMaintenanceTicket(id, date, description, email, assetNumber);
+        // clear textfields
+      if (errorMessage.isEmpty()) {
+        updateTicketIdTextField.setText(null);
+        updateTicketDateTextField.setText(null);
+        updateTicketDescriptionTextField.setText(null);
+        updateTicketRaiserEmailTextField.setText(null);
+        updateTicketAssetNumberTextField.setText(null);
+
+      } else {
+        ViewUtils.showError(errorMessage);
+      }
 
 
-  } 
+    }
+   }
+  // Event Listener on Button[#deleteButton].onAction
+  @FXML
+  public void deleteTicket(ActionEvent event) {
+    String idString = updateTicketIdTextField.getText();
+
+  }
 }
 
