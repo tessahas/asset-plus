d14a4d754ba9448c360c67c9220a86e8ea1d85b1
Tessa Hason <tessa.hason@mail.mcgill.ca>
2023-12-03 21:26:48 -0500

    Javadoccccc View Util$$$$

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ViewUtils.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ViewUtils.java
@@ -14,9 +14,21 @@ import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+/**
+ * <h1>ViewUtils</h1>
+ * Utility class for handling views and displaying messages.
+ * 
+ * @author Tessa Hason, Yuri Sorice, Jerome Desrosiers, Mathieu Allaire, Luis Jarquin, Kevin Liu
+ */
 public class ViewUtils {
 
-  /** Calls the controller and shows an error, if applicable. */
+  /**
+   * Calls the controller and shows an error, if applicable.
+   * 
+   * @param result The result from the controller.
+   * @return True if the call to the controller is successful; otherwise, false.
+   * @author Tessa Hason
+   */
   public static boolean callController(String result) {
     if (result.isEmpty()) {
       AssetPlusFxmlView.getInstance().refresh();
@@ -26,7 +38,13 @@ public class ViewUtils {
     return false;
   }
 
-  /** Calls the controller and returns true on success. This method is included for readability. */
+  /**
+   * Calls the controller and returns true on success. This method is included for readability.
+   * 
+   * @param controllerResult The result from the controller.
+   * @return True if the call to the controller is successful; otherwise, false.
+   * @author Jerome Desrosiers
+   */
   public static boolean successful(String controllerResult) {
     return callController(controllerResult);
   }
@@ -36,6 +54,7 @@ public class ViewUtils {
    *
    * @param title: title of the popup window
    * @param message: message to display
+   * @author Luis Jarquin
    */
   public static void makePopupWindow(String title, String message) {
     Stage dialog = new Stage();
@@ -59,11 +78,21 @@ public class ViewUtils {
     dialog.setTitle(title);
     dialog.show();
   }
-
+  /**
+   * Displays an error message in a popup window.
+   *
+   * @param message The error message to display.
+   * @author Yuri Sorice
+   */
   public static void showError(String message) {
     makePopupWindow("Error", message);
   }
-
+  /**
+   * Gets the maintenance tickets.
+   *
+   * @return An ObservableList containing all maintenance tickets.
+   * @author Mathieu Allaire, Kevin Liu
+   */
   public static ObservableList<TOMaintenanceTicket> getTickets() {
     List<TOMaintenanceTicket> TOMaintenanceTickets = AssetPlusFeatureSet6Controller.getTickets();
     // as javafx works with observable list, we need to convert the java.util.List to
